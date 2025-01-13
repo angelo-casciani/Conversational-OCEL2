4d67163c2049126b28b31530c8cfa33e564fcc11
AngeloC99 <angelocasciani@live.it>
2025-01-13 07:24:49 +0000

    Validation Results and Commands

--- src/cmd4tests.sh
+++ src/cmd4tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # python3 src/main.py --modality live --rebuild_db False --llm_id meta-llama/Meta-Llama-3.1-8B-Instruct --max_new_tokens 768
-python3 src/main.py --modality evaluation-global --rebuild_db True --llm_id meta-llama/Meta-Llama-3.1-8B-Instruct --max_new_tokens 768
+python3 src/main.py --modality evaluation-global --rebuild_db False --llm_id meta-llama/Meta-Llama-3.1-8B-Instruct --max_new_tokens 768
 python3 src/main.py --modality evaluation-events --rebuild_db False --llm_id meta-llama/Meta-Llama-3.1-8B-Instruct --max_new_tokens 768
 python3 src/main.py --modality evaluation-objects --rebuild_db False --llm_id meta-llama/Meta-Llama-3.1-8B-Instruct --max_new_tokens 768
 python3 src/main.py --modality evaluation-ts --rebuild_db False --llm_id meta-llama/Meta-Llama-3.1-8B-Instruct --max_new_tokens 768
